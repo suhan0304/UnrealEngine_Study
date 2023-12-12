c6e6ff1be1131ba6d96d471bd9652ba7e378dd69
ZerrY <suhan0304@gmail.com>
2023-12-12 22:04:55 +0900

    Update Test2.py

--- GameProjectReport/Test2.py
+++ GameProjectReport/Test2.py
@@ -1,4 +1,5 @@
 from vpython import *
+import random
 
 # 씬 설정
 scene = canvas()
@@ -14,7 +15,10 @@ sphere_radius = 0.05  # 구체의 반지름
 cylinder = cylinder(pos=vector(0, 0, 0), axis=vector(0, 0, cylinder_height), radius=cylinder_radius, opacity=0.3)
 
 # 구체를 최대한 배치하여 원기둥 공간 채우기
-while True:
+num_spheres = 0
+max_spheres = 500
+
+while num_spheres < max_spheres:
     x = random.uniform(-cylinder_radius, cylinder_radius)
     y = random.uniform(-cylinder_radius, cylinder_radius)
     z = random.uniform(0, cylinder_height)
@@ -23,5 +27,6 @@ while True:
     if x**2 + y**2 <= cylinder_radius**2:
         # 구체 생성
         sphere(pos=vector(x, y, z), radius=sphere_radius, color=color.blue)
+        num_spheres += 1
 
     rate(100)  # 배치 속도를 적절히 조절
