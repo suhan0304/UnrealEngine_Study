8e07e44f32e20789e9472b752b07fa5cf169ab7c
ZerrY <suhan0304@gmail.com>
2023-12-12 22:01:37 +0900

    Update Test2.py

--- GameProjectReport/Test2.py
+++ GameProjectReport/Test2.py
@@ -1,23 +1,26 @@
 from vpython import *
-import math
 
-# 씬 생성
+# 씬 설정
 scene = canvas()
 
-# 원기둥의 속성
-num_spheres = 1000  # 구체의 개수
-radius = 0.1  # 구체의 반지름
-cylinder_radius = 5  # 원기둥의 반지름
-cylinder_height = 10  # 원기둥의 높이
+# 원기둥의 속성 정의
+num_layers = 10  # 전선의 높이를 구성하는 구체의 층 수
+num_beads_per_layer = 20  # 각 층당 구체의 수
+radius = 1  # 전선의 반지름
 
-# 구체들을 담을 리스트
-spheres = []
+# 구체들을 원기둥 표면에 채워 넣어 전선 생성
+for i in range(num_layers):
+    layer_theta = 2 * pi / num_beads_per_layer
 
-# 각도에 따른 원기둥 표면상의 점을 생성하고 그 위치에 구체를 배치
-for i in range(num_spheres):
-    theta = i * (2 * pi / num_spheres)
-    x = cylinder_radius * math.cos(theta)
-    y = cylinder_radius * math.sin(theta)
-    z = cylinder_height * (i / num_spheres) - cylinder_height / 2
-    sphere_object = sphere(pos=vector(x, y, z), radius=radius, color=color.blue)
-    spheres.append(sphere_object)
+    for j in range(num_beads_per_layer):
+        theta = j * layer_theta
+        x = radius * cos(theta)
+        y = radius * sin(theta)
+        z = i * (2 * radius / num_layers) - radius  # 높이를 조절하여 원기둥 표면에 배치
+
+        # 구체 생성
+        bead = sphere(pos=vector(x, y, z), radius=0.05, color=color.blue)
+
+# VPython 루프 시작
+while True:
+    rate(60)
