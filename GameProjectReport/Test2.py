f70b307b85255da00fd00a34d444b674ee050b21
ZerrY <suhan0304@gmail.com>
2023-12-12 20:54:39 +0900

    Update Test2.py

--- GameProjectReport/Test2.py
+++ GameProjectReport/Test2.py
@@ -4,12 +4,21 @@ import random
 # 씬 생성
 scene = canvas()
 
-# 구체 생성
-sphere_object = sphere(pos=vector(random.uniform(-5, 5), random.uniform(-5, 5), random.uniform(-5, 5)),
-                       radius=1, color=color.blue)
+# 원통의 속성
+num_spheres = 100  # 구체의 개수
+radius = 1  # 구체의 반지름
+cylinder_radius = 5  # 원통의 반지름
 
-# 랜덤한 속도 생성
-velocity = vector(random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1))
+# 구체들을 담을 리스트
+spheres = []
+
+# 구체들을 랜덤한 위치에 생성
+for _ in range(num_spheres):
+    sphere_object = sphere(pos=vector(random.uniform(-cylinder_radius, cylinder_radius),
+                                      random.uniform(-cylinder_radius, cylinder_radius),
+                                      random.uniform(-cylinder_radius, cylinder_radius)),
+                           radius=radius, color=color.blue)
+    spheres.append(sphere_object)
 
 # 프레임 간격 설정
 dt = 0.01
@@ -17,13 +26,4 @@ dt = 0.01
 while True:
     rate(100)  # 초당 100회 업데이트
 
-    # 현재 위치에 속도를 더함
-    sphere_object.pos += velocity * dt
-
-    # 벽에 닿았을 경우 속도를 반전
-    if abs(sphere_object.pos.x) > 5:
-        velocity.x *= -1
-    if abs(sphere_object.pos.y) > 5:
-        velocity.y *= -1
-    if abs(sphere_object.pos.z) > 5:
-        velocity.z *= -1
+    # 더 이상 이동 코드가 없음 (구체들은 정적으로 고정됨)
