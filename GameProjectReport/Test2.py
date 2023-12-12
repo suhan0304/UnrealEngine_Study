174d5d06756519104a62be501eddec1489485e1e
ZerrY <suhan0304@gmail.com>
2023-12-12 22:08:05 +0900

    Update Test2.py

--- GameProjectReport/Test2.py
+++ GameProjectReport/Test2.py
@@ -5,8 +5,8 @@ import random
 scene = canvas()
 
 # 원기둥의 속성 정의
-cylinder_radius = 1  # 원기둥의 반지름
-cylinder_height = 3  # 원기둥의 높이
+cylinder_radius = 0.5  # 원기둥의 반지름
+cylinder_height = 2  # 원기둥의 높이
 
 # 구체의 속성 정의
 sphere_radius = 0.05  # 구체의 반지름
@@ -25,8 +25,16 @@ while num_spheres < max_spheres:
 
     # 원기둥 내부에 있는지 확인
     if x**2 + y**2 <= cylinder_radius**2:
-        # 구체 생성
-        sphere(pos=vector(x, y, z), radius=sphere_radius, color=color.blue)
-        num_spheres += 1
+        # 현재 위치에서 다른 구체들과 겹치지 않는지 확인
+        overlap = False
+        for obj in scene.objects:
+            if isinstance(obj, sphere) and mag(vector(x, y, z) - obj.pos) < (sphere_radius + obj.radius):
+                overlap = True
+                break
 
-    rate(100)  # 배치 속도를 적절히 조절
+        # 겹치지 않는 경우에만 구체 생성
+        if not overlap:
+            sphere(pos=vector(x, y, z), radius=sphere_radius, color=color.blue)
+            num_spheres += 1
+
+    rate(20)  # 배치 속도를 적절히 조절
